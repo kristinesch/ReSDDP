6be759ea49ed51fc668808b7dc2674dc78cb907e
Kristine Schüller <krisch@ntnu.no>
2025-02-21 14:21:49 +0100

    added areas_with_feas_cuts optional argument

--- run_example.jl
+++ run_example.jl
@@ -47,6 +47,8 @@ config = YAML.load_file("config.yaml")
 calculate_feasibility_cuts = true #set to false if already done, needs to be true for first run
 detailed_sim = true
 save_strategy_to_file = true
+
+
 if (config["calculate_feasibility_cuts"] == "false")
     calculate_feasibility_cuts = false
 end
@@ -57,16 +59,15 @@ if (config["save_strategy_to_file"] == "false")
     save_strategy_to_file = false
 end
 
-if (LFeasCut == true)
-    if (LFeasPerStage == false)
+if LFeasCut == true
+    if !LFeasPerStage == true
         label = label*"-feas1"
-    end
     else
         label = label*"-feasN"
+    end
 end
 
 label = label*"-"*string(NScen)*"-"*string(NK)
-
 
 #set paths to input data and result folder from config file
 
@@ -88,6 +89,13 @@ mkpath(resultpath)
 model = load(datapath, parameters) 
 inflow_model = load_inflow(datapath, model, parameters)
 
+println(model.AreaName)
+#areas_to_ignore = ["NUMEDAL", "TERM"]
+
+areas_with_feas_cuts = config["areas_with_feas_cuts"]
+#areas_with_feas_cuts = ["OSTLAND", "SOROST", "HALLINGDAL", "TELEMARK", "SORLAND", "VESTSYD", "VESTMIDT", "NORGEMIDT", "HELGELAND", "TROMS", "FINNMARK"]
+#areas_to_ignore = ["SVER-ON1", "SVER-ON2", "SVER-NN1", "SVER-NN2", "SVER-MIDT", "SVER-SYD", "FINLAND", "DANM-OST", "DANM-VEST", "TYSK-OST", "TYSK-NORD", "TYSK-MIDT", "TYSK-SYD", "TYSK-SVEST", "TYSK-VEST", "TYSK-IVEST", "NEDERLAND", "BELGIA", "GB-SOUTH", "GB-MID", "GB-NORTH", "NORGEM-OWP", "VESTMI-OWP", "VESTSY-OWP", "SORLAN-OWP", "SVER-N-OWP", "SVER-M-OWP", "SVER-S-OWP", "FI-OWP", "DANM-O-OWP", "DANM-V-OWP", "TYSK-O-OWP", "TYSK-V-OWP", "NEDERL-OWP", "BELGIA-OWP", "GB-N-OWP", "GB-M-OWP", "GB-S-OWP", "FRANKRIKE", "SVEITS", "OSTERRIKE", "TSJEKKIA", "POLEN", "BALTIC", "DENMARK-H2", "FINLAND-H2", "FRANCE-H2", "GB-H2", "NETHERLANDS-H2", "POLAND-H2", "SWEDEN-H2", "CZE-H2", "GERMANY-H2", "BALTIC-H2", "H2-M", "H2-S", "H2-N", "SWITZERLAND-H2", "AUSTRIA-H2", "BELGIUM-H2", "TRANSIT-H2", "HELGEL-OWP", "TROMS-OWP", "FINNMA-OWP", "BALTIC-OWP", "FRANKR-OWP", "POLEN-OWP"]
+
 using JLD2 
 using FileIO 
 
@@ -98,7 +106,7 @@ file = File(format"JLD2", joinpath(@__DIR__, "feas_spaces.jld2"))
 if (calculate_feasibility_cuts)
     #Compute feasibility cuts 
     println("Compute feasibility cuts..")
-    feas_spaces = feasibility(model, inflow_model, parameters, datapath; optimizer=optimizer)
+    feas_spaces = feasibility(model, inflow_model, parameters, datapath; optimizer=optimizer, areas_with_feas_cuts = areas_with_feas_cuts)
     save(file, "feas_spaces", feas_spaces)
    
 end
@@ -135,11 +143,15 @@ println("Start simulation ..")
 results_agg = simulate_aggregated(model, inflow_model, parameters, strategy, feas_spaces, init_val)
 
 # Print results to ASCII files 
-println("Write results ..")
+println("Writing results to "*resultpath)
 print_results(resultpath,results_agg,model.NArea,model.NHSys,parameters.Control.NScenSim,parameters.Control.NStageSim,parameters.Time.NK,model.NLine,parameters.Time)
 print_dims(resultpath,model.NHSys,parameters.Control.NStage,parameters.Control.NScenSim,strategy.NCut,parameters.Control.MaxIter,parameters.Control.CCMaxIter)
 print_strategy(resultpath,strategy,parameters.Control.LCostApprox)
 print_feas(resultpath,feas_spaces[1],model.NHSys)
+
+open(joinpath(resultpath,"NScen.txt"), "w") do file
+    write(file, string(NScen))
+end
 
 if (detailed_sim)
     println("Start detailed simulation ..")
